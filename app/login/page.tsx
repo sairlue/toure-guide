9073868ef7fbe4af14a6e886d4d73a37d37753d1
Oliver <oliver@visibleone.com>
2025-08-22 10:01:29 +0700

    fix login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,49 @@
 "use client";
-import { useState } from "react";
-
+import api from "@/lib/api";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 function page() {
+  const [message, setMessage] = useState("");
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+    if (loggedIn) {
+      router.push("/");
+    }
+  }, [router]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     // Handle registration logic here
-    alert(`Registered with\nName: ${form.password}\nEmail: ${form.email}`);
+    try {
+      const res = await api.post("/login", form);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("isLoggedIn", "true");
+      router.push("/");
+      setMessage("Registration successful ✅");
+      setIsLoading(false);
+    } catch (e) {
+      setMessage(e + "Error registering ❌");
+      setIsLoading(false);
+    }
   };
   return (
     <div className="mx-auto max-w-md mt-10 p-6 border rounded shadow">
       <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
       <h4 className="mb-6 text-center">You can login here for tour guide</h4>
+      {message && <p className="text-center text-red-600 mb-4">{message}</p>}
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
@@ -41,11 +65,21 @@ function page() {
         />
         <button
           type="submit"
-          className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+          disabled={isLoading}
+          className={cn(
+            "bg-blue-600 text-white py-2 rounded hover:bg-blue-700",
+            isLoading && "bg-slate-300"
+          )}
         >
-          Register
+          {isLoading ? "Login in..." : "Login"}
         </button>
       </form>
+      <div className="mt-4 text-center text-sm text-gray-600">
+        If you don't have account{" "}
+        <Link href="/register" className="text-blue-600 hover:underline">
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,10 @@
 "use client";
-import { useState } from "react";
+
 import api from "@/lib/api";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
 function page() {
   const [message, setMessage] = useState("");
   const [form, setForm] = useState({
@@ -9,20 +13,33 @@ function page() {
     email: "",
     password: "",
   });
-
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+    if (loggedIn) {
+      router.push("/");
+    }
+  }, [router]);
+  //const { returnUrl } = router.query;
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     // Handle registration logic here
     try {
       const res = await api.post("/register", form);
       localStorage.setItem("token", res.data.token);
+      localStorage.setItem("isLoggedIn", "true");
+      router.push("/");
       setMessage("Registration successful ✅");
-    } catch {
-      setMessage("Error registering ❌");
+      setIsLoading(false);
+    } catch (e) {
+      setMessage(e + "Error registering ❌");
+      setIsLoading(false);
     }
   };
   return (
@@ -31,6 +48,7 @@ function page() {
         Register / Sign Up
       </h1>
       <h4 className="mb-6 text-center">You can register here for tour guide</h4>
+      {message && <p className="text-center text-red-600 mb-4">{message}</p>}
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -68,13 +86,24 @@ function page() {
           className="border p-2 rounded"
           required
         />
+
         <button
+          disabled={isLoading}
           type="submit"
-          className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+          className={cn(
+            "bg-blue-600 text-white py-2 rounded hover:bg-blue-700",
+            isLoading && "bg-slate-300"
+          )}
         >
-          Register
+          {isLoading ? "Register in..." : "Register"}
         </button>
       </form>
+      <div className="mt-4 text-center text-sm text-gray-600">
+        If you have account{" "}
+        <Link href="/login" className="text-blue-600 hover:underline">
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
